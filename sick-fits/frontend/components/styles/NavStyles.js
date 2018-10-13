b78ffc6a0d106b6e61358cf636742b6327dfd15f
Steven Washington <washington.steven@gmail.com>
2018-10-13 12:03:24 -0400

    Module 2 complete

--- sick-fits/frontend/components/styles/NavStyles.js
+++ sick-fits/frontend/components/styles/NavStyles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const NavStyles = styled.ul`
   margin: 0;
@@ -23,9 +23,9 @@ const NavStyles = styled.ul`
       padding: 0 10px;
     }
     &:before {
-      content: '';
+      content: "";
       width: 2px;
-      background: ${props => props.theme.lightgrey};
+      background: ${props => props.theme.lightGrey};
       height: 100%;
       left: 0;
       position: absolute;
@@ -36,7 +36,7 @@ const NavStyles = styled.ul`
     &:after {
       height: 2px;
       background: red;
-      content: '';
+      content: "";
       width: 0;
       position: absolute;
       transform: translateX(-50%);
