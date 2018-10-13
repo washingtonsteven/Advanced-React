ba59654853edaa3d3f1cd1bbe0e7eb9327cf1793
Steven Washington <washington.steven@gmail.com>
2018-10-13 19:45:15 -0400

    Setup Mutations and Queries - Video 15

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,14 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, context, info) {
+    // TODO: check if they are logged in
+
+    const item = await context.db.mutation.createItem(
+      { data: { ...args } },
+      info
+    );
+
+    return item;
+  }
+};
 
 module.exports = Mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,13 @@
-const Query = {};
+const { forwardTo } = require("prisma-binding");
+
+const Query = {
+  items: forwardTo("db") // db is part of the `context`
+
+  // full function below, but since items can be passed through to prisma, we can "forward it"
+  // async items(parent, args, context, info) {
+  //   const items = await context.db.query.items();
+  //   return items;
+  // }
+};
 
 module.exports = Query;
