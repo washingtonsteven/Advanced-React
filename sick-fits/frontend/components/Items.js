5ce5920e2bca1ffc77346dbde132d06deee1d23e
Steven Washington <washington.steven@gmail.com>
2018-10-13 20:27:28 -0400

    Pulling iamges from Apollo/GraphQL - Video 16

---
+++ sick-fits/frontend/components/Items.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import styled from "styled-components";
+import Item from "../components/Item";
+
+const ALL_ITEMS_QUERY = gql`
+  query ALL_ITEMS_QUERY {
+    items {
+      id
+      title
+      price
+      description
+      image
+      largeImage
+    }
+  }
+`;
+
+const Center = styled.div`
+  text-align: center;
+`;
+
+const ItemsList = styled.div`
+  display: grid;
+  grid-gap: 60px;
+  grid-template-columns: 1fr 1fr;
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+`;
+
+class Items extends Component {
+  render() {
+    return (
+      <div>
+        <p>Items!</p>
+        <Query query={ALL_ITEMS_QUERY}>
+          {({ data, error, loading }) => {
+            if (loading) return <p>Loading...</p>;
+            if (error) return <p>Error: {error.message}</p>;
+
+            return (
+              <ItemsList>
+                <div>
+                  {data.items.map(item => (
+                    <Item item={item} key={item.id} />
+                  ))}
+                </div>
+              </ItemsList>
+            );
+          }}
+        </Query>
+      </div>
+    );
+  }
+}
+
+export default Items;
